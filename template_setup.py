f5afb37d49ee0b2e43ebc2216294df33ec7d3ac7
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-03-24 11:19:02 +0300

    fix: some naming bugs also add vscode settings

--- template_setup.py
+++ template_setup.py
@@ -1,31 +1,43 @@
+"""This module is to setup the new project."""
+
 import subprocess
 from pathlib import Path
 
 CURRENT_PROJECT_PATH = Path(__file__).parent
 
-FILE_PATHS = (
-    ["pyproject.toml", "README_main.md"]
-    + [str(path) for path in (CURRENT_PROJECT_PATH / "src").rglob("*.py")]
-    + [str(path) for path in (CURRENT_PROJECT_PATH / "tests").rglob("*.py")]
-)
-DIR_PATHS = [str(CURRENT_PROJECT_PATH / "src" / "template")]
-
 USER_REPLACE_PATHS = ["pyproject.toml", "README_main.md"]
+SRC_PATHS = [str(path) for path in (CURRENT_PROJECT_PATH / "src").rglob("*.py")]
+TESTS_PATHS = [str(path) for path in (CURRENT_PROJECT_PATH / "tests").rglob("*.py")]
+
+FILE_PATHS = USER_REPLACE_PATHS + SRC_PATHS + TESTS_PATHS
+DIR_PATHS = [str(CURRENT_PROJECT_PATH / "src" / "template_python")]
 
 
 def rename_files(
     old_word: str, new_word: str, file_paths: list[str], dir_paths: list[str]
 ) -> None:
+    """Renames files and directories.
+
+    Args:
+        old_word (str)
+        new_word (str)
+        file_paths (list[str])
+        dir_paths (list[str])
+    """
     file_paths = [CURRENT_PROJECT_PATH / path for path in file_paths]
     dir_paths = [CURRENT_PROJECT_PATH / path for path in dir_paths]
+
     for path in file_paths:
-        with open(path, encoding="utf-8") as f:
-            contents = f.read()
+        with open(path, encoding="utf-8") as file:
+            contents = file.read()
         new_contents = contents.replace(old_word, new_word)
-        with open(path, "w", encoding="utf-8") as f:
-            f.write(new_contents)
+
+        with open(path, "w", encoding="utf-8") as file:
+            file.write(new_contents)
+
         new_path = path.with_name(path.name.replace(old_word, new_word))
         path.rename(new_path)
+
     for path in dir_paths:
         new_path = path.with_name(
             path.name.replace(old_word, new_word)
@@ -33,12 +45,14 @@ def rename_files(
         path.rename(new_path)
 
 
-if __name__ == "__main__":
+def main() -> None:
+    """Main function that setups the project."""
     # Rename file contents, names, and directories
-    rename_files("template", CURRENT_PROJECT_PATH.name.replace("-", "_"), FILE_PATHS, DIR_PATHS)
+    rename_files("template_python", CURRENT_PROJECT_PATH.name.replace("-", "_"), FILE_PATHS, DIR_PATHS)
+    rename_files("template-python", CURRENT_PROJECT_PATH.name, USER_REPLACE_PATHS, [])
     while True:
-        new_user_name = input("Enter Github username: ")
-        new_user_email = input("Enter Github email: ")
+        new_user_name = input("Enter GitHub username: ") or "oedokumaci"
+        new_user_email = input("Enter GitHub email: ") or "oral.ersoy.dokumaci@gmail.com"
         answer = (
             input(
                 f"Is the information correct? (y/n) {new_user_name} {new_user_email} "
@@ -65,14 +79,37 @@ if __name__ == "__main__":
 
     # Run pdm install
     try:
-        subprocess.run(["pdm", "--version"])
-    except FileNotFoundError:
+        subprocess.check_call(["pdm", "--version"])
+    except subprocess.CalledProcessError:
         try:
-            subprocess.run(["pip3", "install", "pdm"])
-        except FileNotFoundError:
-            subprocess.run(["pip", "install", "pdm"])
+            subprocess.check_call(["pip3", "install", "pdm"])
+        except subprocess.CalledProcessError:
+            subprocess.check_call(["pip", "install", "pdm"])
     # make __pypackages__ directory
     (CURRENT_PROJECT_PATH / "__pypackages__").mkdir(exist_ok=True)
-    subprocess.run(["pdm", "lock", "-v"])
-    subprocess.run(["pdm", "install"])
-    subprocess.run(["pdm", "run", "pre-commit", "install"])
+    subprocess.check_call(["pdm", "lock", "-v"])
+    subprocess.check_call(["pdm", "install"])
+    subprocess.check_call(["pdm", "run", "pre-commit", "install"])
+
+    # Generate .vscode/settings.json
+    answer = input("Do you want to generate .vscode/settings.json? (y/n) ") or "y"
+    if answer == "y":
+        (CURRENT_PROJECT_PATH / ".vscode").mkdir(exist_ok=True)
+        file = (CURRENT_PROJECT_PATH / ".vscode" / "settings.json")
+        file.touch()
+        with open(file, "w", encoding="utf-8") as f:
+            f.write(
+                """{
+                    "python.linting.pylintEnabled": false,
+                    "python.linting.enabled": true,
+                    "python.linting.flake8Enabled": true,
+                    "python.linting.flake8Args": ["--max-line-length=88", "--select=C,E,F,W,B", "--extend-ignore=B009,E203,E501,W503"],
+                    "python.autoComplete.extraPaths": ["__pypackages__/3.10/lib"],
+                    "python.analysis.extraPaths": ["__pypackages__/3.10/lib"],
+                    "python.testing.pytestPath": "__pypackages__/3.10/bin/pytest"
+                }"""
+            )
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
