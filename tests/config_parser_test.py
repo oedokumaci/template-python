9c29fdff9053ed8f9bd10ec88650d5734fb618b4
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-04-22 14:26:55 +0300

    refactor: config_parser_test.py

--- tests/config_parser_test.py
+++ tests/config_parser_test.py
@@ -3,13 +3,19 @@ import pytest
 from template_python.config_parser import YAMLConfig
 
 
-def test_valid_yaml_config() -> None:
-    """Test if the valid YAML config file is parsed and validated correctly."""
-    config = YAMLConfig(log_file_name="test.log")
-    assert config.log_file_name == "test.log"
-
-
-def test_invalid_yaml_config() -> None:
-    """Test if an invalid YAML config file raises a ValueError."""
-    with pytest.raises(ValueError):
-        YAMLConfig(log_file_name="test")
+@pytest.mark.parametrize(
+    "log_file_name, expected",
+    [
+        ("valid_extension_test.log", "valid_extension_test.log"),
+        ("invalid_extension_test", ValueError),
+        ("invalid_extension_test.csv", ValueError),
+    ],
+)
+def test_parse_yaml_config(log_file_name: str, expected: str | ValueError) -> None:
+    """Test if the YAML config file is parsed and validated correctly."""
+    if isinstance(expected, type) and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            YAMLConfig(log_file_name=log_file_name)
+    else:
+        config = YAMLConfig(log_file_name=log_file_name)
+        assert config.log_file_name == expected
